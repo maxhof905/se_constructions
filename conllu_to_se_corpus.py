e59d143eab7a1e28b7a0a675aa2f40c2e117c5e8
Maxine Hofstetter <Maxine.hofstetter@uzh.ch>
2022-03-17 20:59:18 +0100

    process conllu

--- conllu_to_se_corpus.py
+++ conllu_to_se_corpus.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import csv
+import os
 #%%
 
 
@@ -9,8 +10,9 @@ def get_se_dict(infile):
     se_dict = dict()
     new_line = '\n'
     se_sentences = []
+    sentence_index = 0
 
-    with open(infile) as file:
+    with open(infile, 'r', encoding='utf-8') as file:
         for line in file:
             if line != new_line:
                 lines.append(line)
@@ -27,33 +29,55 @@ def get_se_dict(infile):
             counter = 0
             for elem in sentence:
                 if elem.startswith(('# sent_id =')):
-                    sent_id = elem.strip('# sent_id =').rstrip()
+                    corpus_id = elem.strip('# sent_id =').rstrip()
                 if elem.startswith('# text ='):
-                    # text = elem.strip('# text =').rstrip().replace(',', '')
-                    text = elem.strip('# text =').rstrip().replace('«', '').replace('»', '').replace('-- ', '').replace('(', '').replace(')', '').replace('\'', '')
+                    # replace supra segmental characters (parenthesis and commas influence argument structure > kept)
+                    # dashes are inconsistently > eliminated
+                    text = elem.strip('# text =').rstrip().replace('«', '').replace('»', '').replace('-- ', '').replace('\'', '').replace('"', '').replace('- -', '')
 
-                if 'se\tse' in elem.lower(): #todo match more precisely: se él cases are lost
+                if '\tse\t' in elem.lower(): #match the word column of conllu format
                     counter += 1
-                    se_tag = elem.split('\t')[7]
+                    dep_tag = elem.split('\t')[7]
+                    # pos_tag = elem.split('\t')[4]  # all labeled as p0000000
 
-            if counter == 1:
-                se_dict[sent_id] = [text, se_tag]
+            if counter == 1: # eliminate sentences where se occurs twice
+                sentence_index += 1
+                se_dict[sentence_index] = [corpus_id, text, dep_tag]
 
     return se_dict
-#%%
-filename = 'es_ancora-ud-train.conllu'
-outfile = filename[:-7]+'.txt'
-stats_file = filename[:-7]+'_stats.txt'
-df = pd.DataFrame.from_dict(get_se_dict(filename), orient='index')
-print(df.shape)
-# # df.index.rename('sent_id', inplace=True)
-# # df = df[['sent_id', 'sent', 'tag']]
-# # df.to_csv(outfile, header=False, quoting=csv.QUOTE_NONE, quotechar="",  escapechar="\\")
-# df.to_csv(outfile, header=False, sep='\t')
-# df.value_counts().to_csv(stats_file)
 
-#todo eliminate sentences that contain se twice
 #%%
 
+directory = "ud/conllu"
+data = []
+for root, dirs, files in os.walk(directory, topdown=False):
+    for file in files:
+        filename = (os.path.join(root, file))
+        if 'es_' in filename:
+            out_dir = 'ud/es_data/'
+            if not os.path.isdir(out_dir):
+                os.makedirs(out_dir)
+            outfile = out_dir + file[:-7] + '.txt'
 
+        if 'pt_' in filename:
+            out_dir = 'ud/pt_data/'
+            if not os.path.isdir(out_dir):
+                os.makedirs(out_dir)
+            outfile = out_dir + file[:-7] + '.txt'
+
+        # print(outfile)
+# filename = 'ud/pt_bosque/pt_bosque-ud-train.conllu'
+
+        df = pd.DataFrame.from_dict(get_se_dict(filename), orient='index', columns=['corpus_id', 'text', 'dependency_tag'])
+        print('\n')
+        print(file)
+        print(df.dependency_tag.value_counts())
+        print(df.shape)
+
+        # # print(df.shape, outfile)
+        df.to_csv(outfile, header=False, sep='\t',  encoding='utf-8')
+        #
+        # print('\n', outfile, '\n', df[:-1].value_counts(), '\n') # keep track of the dependency tags used)
+
+#%%
 
