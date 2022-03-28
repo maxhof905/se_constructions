085fb567b82b84f75573e65d03dc3019944f317f
Maxine Hofstetter <Maxine.hofstetter@uzh.ch>
2022-03-28 18:38:40 +0200

    obtain inter annotator agreement

--- ud_iaa.py
+++ ud_iaa.py
@@ -1,28 +1,49 @@
 import random
-from sklearn.metrics import cohen_kappa_score
+from sklearn.metrics import cohen_kappa_score, confusion_matrix
+import numpy as np
+import matplotlib.pyplot as plot
+from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import ConfusionMatrixDisplay
+import seaborn as sns
 
-# todo merge together all of gsd and then draw sample
-# todo decide wheteher to show labels to anotator or not
-# todo work with predefines set of labels? -- define the set of possible labels >> se article or ud docs
+# todo no fuciona el python mode
+#%%
+""" spanish: expl:pv, expl:pass, expl:impers, iobj, obj, flat, fixed"""
+""" portuguese: expl:pv, expl:pass, expl:impers, iobj, obj, flat, fixed"""  # todo
+
+
 #%%
 
-def create_sample(infile, outfile_1, outfile_2):
-    with open(infile, 'r') as file:
-        with open(outfile_1, 'w') as sample_file:
-            with open(outfile_2, 'w') as true_file:
-                random_lines = random.sample(file.readlines(), 200)
+def create_sample(infile_1, infile_2, infile_3, outfile_1, outfile_2):
+    """ create annotation file from data splits"""
+    with open(infile_1, 'r') as train, open(infile_2, 'r') as dev, open(infile_3, 'r') as test:
+        with open(outfile_1, 'w') as sample_file, open(outfile_2, 'w') as true_file:
+                train_lines = train.readlines()
+                dev_lines = dev.readlines()
+                test_lines = test.readlines()
+                lines = train_lines + dev_lines + test_lines
+                random_lines = random.sample(lines, 200)
                 for line in random_lines:
                     true_file.write(line) # todo keep or dismiss label
                     sample_file.write(line.split('\t')[1] + '\n')
-    return infile, outfile_1, outfile_2
+    return None
+
 #%%
 
-es_gsd = create_sample('ud/es_data/es_gsd-ud-train.txt', 'ud/samples/es_gsd-ud-train-sample.txt', 'ud/samples/es_gsd-ud-train-true.txt')
+es_train = 'ud/es_data/es_ancora-ud-train.txt'
+es_dev = 'ud/es_data/es_ancora-ud-dev.txt'
+es_test = 'ud/es_data/es_ancora-ud-test.txt'
+es_sample = 'ud/samples/es_ancora-ud-sample.txt'
+es_true =  'ud/samples/es_ancora-ud-true.txt'
+# create_sample(es_train, es_dev, es_test, es_sample, es_true)
 
-# es_ancora = create_sample('ud/es_data/es_ancora_ud_train.txt', 'ud/samples/es_ancora_ud_train_sample.txt')
-#
-# pt_gsd = create_sample('ud/pt_data/pt_gsd_ud_train.txt', 'ud/samples/pt_gsd_ud_train_sample.txt')
-# pt_bosque = create_sample('ud/pt_data/pt_bosque_ud_train.txt', 'ud/samples/pt_bosque_ud_train_sample.txt')
+pt_train = 'ud/pt_data/pt_bosque_ud_train.txt'
+pt_dev = 'ud/pt_data/pt_bosque_ud_dev.txt'
+pt_test = 'ud/pt_data/pt_bosque_ud_test.txt'
+pt_sample = 'ud/samples/pt_bosque-ud-sample.txt'
+pt_true = 'ud/samples/pt_bosque-ud-true.txt'
+# create_sample(pt_train, pt_dev, pt_test, pt_sample, pt_true)
 
 #%%
 
@@ -31,20 +52,27 @@ def get_label_column(infile):
         labels = [line.split('\t')[-1].rstrip() for line in file.readlines()]
     return labels
 
+#%%
+
 
 #%%
 
-y_true = get_label_column(es_gsd[2])
-y_pred = get_label_column(es_gsd[2]) # todo put actual predictions [1]
+
+
+#%%
+#
+es_y_true = get_label_column(es_true)
+es_y_pred = get_label_column(es_sample)
+
+pt_y_true = get_label_column(pt_true)
+pt_y_pred = get_label_column(pt_sample)
 
 # print(y_true)
 # print(y_pred)
-print(cohen_kappa_score(y_true, y_pred))
 
-# >>> from sklearn.metrics import confusion_matrix
-# >>> y_true = [2, 0, 2, 2, 0, 1]
-# >>> y_pred = [0, 0, 2, 2, 0, 2]
-# >>> confusion_matrix(y_true, y_pred)
-# array([[2, 0, 0],
-#        [0, 0, 1],
-#        [1, 0, 2]])
+es_iaa = cohen_kappa_score(es_y_true, es_y_pred)
+pt_iaa = cohen_kappa_score(pt_y_true, pt_y_pred)
+
+#%%
+cm = confusion_matrix(es_y_true, es_y_pred)  # todo
+f = sns.heatmap(cm, annot=True, fmt='d')
